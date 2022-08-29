83214ef940fef0b0208392815902963bdd977f62
d āvis <dlektauers@gmail.com>
2022-08-29 19:42:06 +0300

    :lipstick: Add design to the privacy policy page

--- client/src/routes/(main)/privatuma-politika/+page.svelte
+++ client/src/routes/(main)/privatuma-politika/+page.svelte
@@ -1,22 +1,20 @@
-<script lang="typescript">
-    import CookieNotice from '$lib/components/CookieNotice.svelte';
-</script>
-
 <svelte:head>
     <title>Privātuma politika | Mežgalciema baptistu draudze</title>
 </svelte:head>
 
-<h1>Privātuma politika</h1>
-<h3>Jūsu dati</h3>
-<p>Mēs no Jūsu informācijas saglabājam tikai laikus, kad Jūs apmeklējāt šo saiti. Nevienai trešajai partijai šie dati rokās nenonāk.</p>
+<h1 class="font-title font-bold text-3xl text-slate-900 mb-4">Privātuma politika</h1>
 
-<h3>Sīkdatnes</h3>
-<p>Sīkdatnes ir neliels daudzums datu, ko mājaslapa var saglabāt, piemēram, iestatījumi kaut kādā mājaslapā vai tamlīdzīgi.</p>
+<h3 class="font-title font-bold text-xl text-slate-800 mt-4">Jūsu dati</h3>
+<p>Mēs saglabājam tikai un vienīgi laikus, kad jūs apmeklējāt šo saiti. Nekādi citi dati netiek saglabāti.</p>
+
+<h3 class="font-title font-bold text-xl text-slate-800 mt-4">Sīkdatnes <i>(cookies)</i></h3>
+<p>Sīkdatnes ir neliels daudzums datu, ko mājaslapa var saglabāt, piemēram, mājaslapas iestatījumi. Sīkdatnes ir gandrīz vienīgais veids, kā mājaslapa var jūs
+    atcerēties un zināt, ka jūs esat jūs.</p>
 <p>Mēs sīkdatnes izmantojam pārsvarā statistiskiem nolūkiem, bet arī dažām funkcionālām daļām.</p>
-<p>Ieejot šajā mājaslapā, Jums parādās izvēlne lapas apakšā - vai Jūs vēlaties piekrist vai nepiekrist sīkdatnēm. Ja Jūs nepiekrītat,
+<p>Ieejot šajā mājaslapā, Jums parādās izvēlne lapas apakšā - vai jūs vēlaties piekrist vai nepiekrist sīkdatnēm. Ja jūs nepiekrītat,
     tad tiks saglabāta tikai viena sīkdatne, lai Jums šo izvēlni vairs nerādītu, un mēs nekādas citas sīkdatnes nesaglabāsim.</p>
 
-<h3>Statistika</h3>
-<p>Vienīgie statistiskie dati, kas tiek ievākti, ir laiki, kad Jūs apmeklējāt šo saiti, kā arī, ja Jūs atļaujat sīkdatnes, tad mēs saglabājam
-    īpašu ID. Katram apmeklētājam šis ID ir atšķirīgs, un to mēs izmantojam, lai atšķirtu apmeklētājus.</p>
-<p>Visi statistikas dati bez izņēmuma tiek neatgriezeniski dzēsti 7 dienas pēc to ievākšanas.</p>
+<h3 class="font-title font-bold text-xl text-slate-800 mt-4">Statistika</h3>
+<p>Vienīgie statistiskie dati, kas tiek ievākti, ir laiki, kad jūs apmeklējāt šo saiti, kā arī, ja jūs atļaujat sīkdatnes, īpašs ID, kas tiek saglabāts
+    kā sīkdatne. Katram apmeklētājam šis ID ir atšķirīgs, un mēs to izmantojam, lai atšķirtu apmeklētājus.</p>
+<p>Visi statistikas dati tiek neatgriezeniski dzēsti 7 dienas pēc to ievākšanas.</p>
