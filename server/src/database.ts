270c0480dd439f2ffd935ae22b0a0cfe0535a587
d āvis <dlektauers@gmail.com>
2022-10-03 19:15:13 +0300

    :arrow_up: Update package versions

--- server/src/database.ts
+++ server/src/database.ts
@@ -5,4 +5,4 @@ import { MongoClient } from 'mongodb';
 
 const dbUrl: string = process.env.DB_DEV_URL || ``;
 const dbName: string = `mezgalciems-lv`;
-const client: MongoClient = new MongoClient(dbUrl, { useUnifiedTopology: true });
+const client: MongoClient = new MongoClient(dbUrl);
