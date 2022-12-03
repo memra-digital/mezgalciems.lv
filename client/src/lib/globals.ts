9bc1b2e38bc3192c49dca711b682fbf6427454ee
Dāvis Lektauers <dlektauers@gmail.com>
2022-12-03 13:28:22 +0200

    :sparkles: Last-minute changes

--- client/src/lib/globals.ts
+++ client/src/lib/globals.ts
@@ -1,3 +1,3 @@
-export const apiUrl: string = `http://localhost:4000/graphql`; // `https://mezgalciems-backend.herokuapp.com/graphql`;
+export const apiUrl: string = `https://mezgalciems-lv-backend.onrender.com`;
 export const year: string = `2022`;
 export const version: string = `v3.1`;
